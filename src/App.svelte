0986a194d22d900fdc9c2492db03d430ee4b3a0f
panupong1212312121 <panupong1212312121@gmail.com>
2023-08-20 20:15:41 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Desktop from './lib/Desktop.svelte';
+  import Mobile from './lib/Mobile.svelte';
+  
+  let windowWidth = window.innerWidth;
+  let windowHeight = window.innerHeight;
+
+  const fullscreenHeight = 500;
+
+  const maxWidthDesktop = 1400;
+  const minWidthDesktop = 768;
+
+  const maxWidthMobile = 767;
+  const minWidthMobile = 375; 
+
+  function handleResize() {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+  }
+
+  // Add an event listener for window resize
+  window.addEventListener('resize', handleResize);
+
+</script>
+
+{#if windowHeight >= fullscreenHeight}
+  <main>
+    {#if windowWidth >= minWidthMobile && windowWidth <= maxWidthMobile}
+      <Mobile />
+    {/if}
+    {#if windowWidth >= minWidthDesktop && windowWidth <= maxWidthDesktop}
+      <Desktop />
+    {/if}
+  </main>
+{/if}
+
+
+
---
+++ src/lib/Desktop.svelte
@@ -0,0 +1,67 @@
+<div class="flex items-center justify-center h-screen w-screen font-body"> 
+    <div class="flex flex-row rounded-2xl h-3/5 w-2/3 shadow-2xl">
+      <div class="flex flex-col items-center justify-around bg-background rounded-2xl h-full w-1/2 p-5"> 
+        <div class="p-1">
+          <p class="text-color1 text-xl font-bold">Your Result</p>
+        </div>
+        <div class="flex flex-col items-center justify-center rounded-full bg-gradient-circle w-44 h-44 p-1">
+          <p class="text-white text-6xl font-bold mb-2">76</p>
+          <p class="text-color1 text-sm font-bold">of 100</p>
+        </div> 
+        <div class="flex flex-col items-center justify-around px-10">
+          <p class="text-white text-2xl font-bold pb-3">Great</p>
+          <p class="text-color1 text-sm text-center">You score higher than 65% of the people who have taken these tests.</p>
+        </div>
+      </div>
+      <div class="flex flex-col justify-around rounded-2xl h-full w-1/2 p-5">
+        <div class="w-full p-1">
+          <p class="text-xl font-bold">Summary</p>
+        </div> 
+        <div class="flex flex-col justify-evenly rounded-xl w-full py-1">
+          <div class="flex flex-row justify-between rounded-xl bg-red-100 w-full p-3 my-2">
+            <div class="flex flex-row">
+              <img src="./src/assets/reaction-icon.svg" alt="Icon" width="15" height="15">
+              <p class="text-red px-2 font-bold">Reaction</p> 
+            </div>
+            <div class="flex flex-row">
+              <p class="px-1 font-bold">80</p>
+              <p class="text-gray-500 px-1 font-bold">/ 100</p>
+            </div>
+          </div>
+          <div class="flex flex-row justify-between rounded-xl bg-yellow-100 w-full p-3 my-2">
+            <div class="flex flex-row">
+              <img src="./src/assets/memory-icon.svg" alt="Icon" width="15" height="15">
+              <p class="text-yellow px-2 font-bold">Memory</p> 
+            </div>
+            <div class="flex flex-row">
+              <p class="px-1 font-bold">92</p>
+              <p class="text-gray-500 px-1 font-bold">/ 100</p>
+            </div>
+          </div>
+          <div class="flex flex-row justify-between rounded-xl bg-green-100 w-full p-3 my-2">
+            <div class="flex flex-row">
+              <img src="./src/assets/verbal-icon.svg" alt="Icon" width="15" height="15">
+              <p class="text-green px-2 font-bold">Verbal</p> 
+            </div>
+            <div class="flex flex-row">
+              <p class="px-1 font-bold">61</p>
+              <p class="text-gray-500 px-1 font-bold">/ 100</p>
+            </div>
+          </div>
+          <div class="flex flex-row justify-between rounded-xl bg-blue-100 w-full p-3 my-2">
+            <div class="flex flex-row">
+              <img src="./src/assets/visual-icon.svg" alt="Icon" width="15" height="15">
+              <p class="text-blue px-2 font-bold">Visual</p> 
+            </div>
+            <div class="flex flex-row">
+              <p class="px-1 font-bold">72</p>
+              <p class="text-gray-500 px-1 font-bold">/ 100</p>
+            </div>
+          </div>
+        </div>
+        <button class="rounded-full btn custom-btn w-full text-white">
+          Continue
+        </button>
+      </div>
+    </div>
+  </div>
